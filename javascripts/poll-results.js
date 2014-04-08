2087bf711201b0f5ab11b0f9e501d9cdcd88686c
vladan-arraystudio <vladan@arraystudio.com>
2014-04-08 12:54:20 +0200

    adding project files

---
+++ javascripts/poll-results.js
@@ -0,0 +1,19 @@
+$(window).load(function(){
+
+
+  $('body').on('click', '.results-trigger', function() {
+    $('.poll-vote-list').fadeOut(500);
+    $('.poll-results-list').delay(500).fadeIn(500);
+    $(this).hide();
+    $('.poll-results-list li .percentage').each(function() {
+      var value = $(this).text();
+  
+      $(this).closest('li').find('.nul').removeClass('inner-bar null').addClass('voted');
+      $(this).closest('li').find('.inner-bar').animate({width: value}, 1000);
+      $(this).closest('li').find('.voted').delay('1000').animate({width: value}, 1000);
+      $('.percentage').delay('2000').fadeIn('fast');
+    });
+    return false;
+  });
+
+});
